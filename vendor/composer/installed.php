f96a73530ed75f0bb317ce93cb67bb6598ac6a17
devMunyi <samunyi90@gmail.com>
2021-10-25 17:12:28 +0300

    Updates

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ef054811578c99e0280b6ac856c77ade2dd86e69',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ef054811578c99e0280b6ac856c77ade2dd86e69',
+            'dev_requirement' => false,
+        ),
+    ),
+);
